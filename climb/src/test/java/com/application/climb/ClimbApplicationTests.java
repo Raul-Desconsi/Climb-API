b134adc0e2db6b36278f57e37d26a9ab75381283
Raul Desconsi <rauldesconsivor@gmail.com>
2025-08-25 20:25:36 -0300

    Primeiro commit

---
+++ climb/src/test/java/com/application/climb/ClimbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.climb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

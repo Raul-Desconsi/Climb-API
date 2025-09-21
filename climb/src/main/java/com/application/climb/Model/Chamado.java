92fa413fcb5afa6392ded90dfc8324829bf385e4
Samii_Zanin <samiizanin@gmail.com>
2025-09-21 02:21:38 -0300

    Definindo tipagem das colunas e suas especificações

--- climb/src/main/java/com/application/climb/Model/Chamado.java
+++ climb/src/main/java/com/application/climb/Model/Chamado.java
@@ -2,8 +2,7 @@ package com.application.climb.Model;
 
 import java.time.LocalDateTime;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -13,8 +12,6 @@ import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
-
-
 @Entity
 @Table
 public class Chamado {
@@ -23,16 +20,47 @@ public class Chamado {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
     private Integer id;
+
+    @Column(length =100, nullable = false, unique = false)
     private String motivo;
+
+    @Column(nullable = false, unique = false)
     private LocalDateTime data;
+
+    @Column(length =60, nullable = false, unique = false)
     private String areaAfetada;
+
+    @Column(length =255, nullable = false, unique = false)
     private String descricao;
-    private String status;
+
+    @Column(length =255, nullable = true, unique = false)
     private Funcionario idResponsavelAbertura;
+
+    @Column(length =255, nullable = true, unique = false)
     private Funcionario idResponsavelResolucao;
-    private enum urgencia{Baixa, Media, Alta};
+
+    @Column(length =15, nullable = false, unique = false)
+    private Urgencia urgencia;
+
+    public enum Urgencia {
+        Baixa,
+        Media,
+        Alta
+    }
+
+    @Column(nullable = false, unique = false)
     private Setor idSetor;
-    private enum Status{Analise, Desenvolvimento, Devolvido, Concluido}
+
+    @Column(length= 30, nullable = false, unique = false)
+    private Status status;
+    
+    public enum Status{
+        Analise,
+        Desenvolvimento,
+        Devolvido,
+        Concluido
+    }
 
 }

96f02b14d40239ce1a47fd87f6b68153fdce2e84
Samii_Zanin <samiizanin@gmail.com>
2025-09-26 23:25:40 -0300

    Registro de chamados feito ( Requer refatoração )

--- climb/climb_interface_application/src/events/userHome.js
+++ climb/climb_interface_application/src/events/userHome.js
@@ -17,3 +17,38 @@ document.addEventListener("DOMContentLoaded", () => {
         location.href = 'login.html';
     }
 });
+function callRegisterFlow(maincontent, iframe) {
+  maincontent.innerHTML = "";
+  iframe.src = "/Climb-API/climb/climb_interface_application/src/page/registerFlow.html"; 
+  maincontent.appendChild(iframe);
+}
+
+function chamadosBtn(maincontent) {
+  maincontent.innerHTML = "";
+  const iframe = document.createElement("iframe"); // cria aqui, porque é novo
+  iframe.src = "/Climb-API/climb/climb_interface_application/src/page/ticket.html"; 
+  maincontent.appendChild(iframe);
+}
+
+function accountBtn(maincontent) {
+    maincontent.innerHTML = "";
+    const iframe = document.createElement("iframe"); 
+    iframe.src = "/Climb-API/climb/climb_interface_application/src/page/account.html"; 
+    maincontent.appendChild(iframe);}
+
+document.addEventListener("click", function (event) {
+  const maincontent = document.getElementById("main-content");
+  const iframe = document.createElement("iframe");
+
+  switch (event.target.id) {
+    case "adicionarBtn":
+      callRegisterFlow(maincontent, iframe);
+      break;
+    case "chamadosBtn":
+      chamadosBtn(maincontent, iframe); // chama a função correta
+      break;
+    case "accountBtn":
+        accountBtn(maincontent, iframe);
+        break; // chama a função correta
+  }
+});

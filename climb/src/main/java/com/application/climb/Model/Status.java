7562b445672686cda4a7a87592ca850e594ea919
Samii-Zanin <samiizanin@gmail.com>
2025-10-13 19:07:41 -0300

    Referencia a empresa adicionada em Status e Urgencia

--- climb/src/main/java/com/application/climb/Model/Status.java
+++ climb/src/main/java/com/application/climb/Model/Status.java
@@ -2,10 +2,12 @@ package com.application.climb.Model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -22,5 +24,9 @@ public class Status {
 
     @Column(length = 50, nullable = false)
     private String nome;
-
+   
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "empresa_id", nullable = false)
+    
+    private Empresa empresa;
 }
--- climb/src/main/java/com/application/climb/Model/Urgencia.java
+++ climb/src/main/java/com/application/climb/Model/Urgencia.java
@@ -3,10 +3,12 @@ package com.application.climb.Model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,4 +25,10 @@ public class Urgencia {
 
     @Column(length = 50, nullable = false)
     private String nome;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "empresa_id", nullable = false)
+    
+    private Empresa empresa;
+
 }
